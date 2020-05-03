118b88f0c2dac3ebea90875a9ad826ef3c0e76a5
ostrydiem <t.z.ostrowski@gmail.com>
2020-05-04 01:10:37 +0200

    make it better

--- src/main/java/com/themdtnoauthorization/noauthorization/MdtApplication.java
+++ src/main/java/com/themdtnoauthorization/noauthorization/MdtApplication.java
@@ -1,4 +1,4 @@
-package com.themdtnoauthorization.noauthorization.entity;
+package com.themdtnoauthorization.noauthorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

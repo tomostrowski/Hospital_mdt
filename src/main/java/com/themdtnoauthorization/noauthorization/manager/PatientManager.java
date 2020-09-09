74ff587a2cf87f125a631e447225df1d23b9b788
tomostrowski <t.z.ostrowski@gmail.com>
2020-09-09 22:20:53 +0200

    commented public void fillDB()

--- src/main/java/com/themdtnoauthorization/noauthorization/manager/PatientManager.java
+++ src/main/java/com/themdtnoauthorization/noauthorization/manager/PatientManager.java
@@ -55,13 +55,13 @@ public class PatientManager {
         return diseaseRepo.findAllByPatientOrderByDiagnosisDateDesc(patient);
     }
 
-   @EventListener(ApplicationReadyEvent.class)
-   public void fillDB(){
-        save(new Patient("Weronika", "Rosatti", "AWQ210321", LocalDate.of(1980, 9, 14 ), "F", "+55 564 000 300", "rosati@gmail.com", "6as123345"));
-        save(new Patient( "Magdalena", "Gorys", "TWM21000021", LocalDate.of(1981, 9, 15 ), "F", "+49 253 923 436", "gorys@gmail.com", "969856"));
-        save(new Patient("Bożena", "Maliszewska", "OTWTH6427", LocalDate.of(1958, 10, 15 ), "F", "+22 125 923 111", "maria234@gmail.com", "162346"));
-        save(new Patient("Agnieszka", "Bargos", "OWTSD210321", LocalDate.of(1963, 6, 14 ), "F", "+78 3214 923 111", "goska235@gmail.com", "864824511"));
-        save(new Patient("Natalia", "Olensko", "PTSR210321", LocalDate.of(1972, 1, 14 ), "F", "+88 111 685 972", "barbara@gmail.com", "e503952"));
-        save(new Patient("Tatiana", "Partyka", "KKS210321", LocalDate.of(1962, 9, 14 ), "F", "+411 777 963 436", "mickiewicz@gmail.com", "5234k6427"));
-   }
+//   @EventListener(ApplicationReadyEvent.class)
+//   public void fillDB(){
+//        save(new Patient("Weronika", "Rosatti", "AWQ210321", LocalDate.of(1980, 9, 14 ), "F", "+55 564 000 300", "rosati@gmail.com", "6as123345"));
+//        save(new Patient( "Magdalena", "Gorys", "TWM21000021", LocalDate.of(1981, 9, 15 ), "F", "+49 253 923 436", "gorys@gmail.com", "969856"));
+//        save(new Patient("Bożena", "Maliszewska", "OTWTH6427", LocalDate.of(1958, 10, 15 ), "F", "+22 125 923 111", "maria234@gmail.com", "162346"));
+//        save(new Patient("Agnieszka", "Bargos", "OWTSD210321", LocalDate.of(1963, 6, 14 ), "F", "+78 3214 923 111", "goska235@gmail.com", "864824511"));
+//        save(new Patient("Natalia", "Olensko", "PTSR210321", LocalDate.of(1972, 1, 14 ), "F", "+88 111 685 972", "barbara@gmail.com", "e503952"));
+//        save(new Patient("Tatiana", "Partyka", "KKS210321", LocalDate.of(1962, 9, 14 ), "F", "+411 777 963 436", "mickiewicz@gmail.com", "5234k6427"));
+//   }
 }

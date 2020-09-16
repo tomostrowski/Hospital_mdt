7c57e831de00926e5e85361c802bc8b90243a00a
tomostrowski <t.z.ostrowski@gmail.com>
2020-09-16 09:47:41 +0200

    database

--- src/main/java/com/themdtnoauthorization/noauthorization/entity/MedicalHistory.java
+++ src/main/java/com/themdtnoauthorization/noauthorization/entity/MedicalHistory.java
@@ -20,7 +20,7 @@ public class MedicalHistory {
     private String allergies;
     private String performanceStatus;
 
-    @OneToOne(mappedBy = "medicalHistory", cascade = CascadeType.ALL,
+    @OneToOne(cascade = CascadeType.ALL,
             fetch = FetchType.LAZY, optional = false)
     private Patient patient;
 
